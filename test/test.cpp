37430da95eb9b125e1f48c77390f2b68380bb797
Siddharth Telang <stelang@umd.edu>
2021-10-01 15:52:59 -0400

    Update the tests

--- test/test.cpp
+++ test/test.cpp
@@ -12,24 +12,73 @@
 #include <gtest/gtest.h>
 #include <pid.hpp>
 
+
+TEST(InvalidDt, test_checkParameters) {
+  EXPECT_THROW(PID pid(1.0, 2.0, 3.0, 0.0, 0.0, 0.0), std::domain_error);
+  EXPECT_THROW(PID pid(1.0, 2.0, 3.0, -5.0, 0.0, 0.0), std::domain_error);
+}
+
+TEST(InvalidGains, test_checkParameters) {
+  //  invalid Kp
+  EXPECT_THROW(PID pid(0.0, 2.0, 3.0, 1.0, 0.0, 0.0), std::domain_error);
+  EXPECT_THROW(PID pid(-2.0, 2.0, 3.0, 1.0, 0.0, 0.0), std::domain_error);
+
+  //  invalid Ki
+  EXPECT_THROW(PID pid(1.0, 0.0, 3.0, 1.0, 0.0, 0.0), std::domain_error);
+  EXPECT_THROW(PID pid(1.0, -2.0, 3.0, 1.0, 0.0, 0.0), std::domain_error);
+
+  //  invalid Kd
+  EXPECT_THROW(PID pid(2.0, 2.0, 0.0, 1.0, 0.0, 0.0), std::domain_error);
+  EXPECT_THROW(PID pid(2.0, 2.0, -3.0, 1.0, 0.0, 0.0), std::domain_error);
+}
+
+TEST(ValidGains, test_checkParameters) {
+  //  valid Kp
+  EXPECT_NO_THROW(PID pid(0.2, 2.0, 3.0, 1.0, 0.0, 0.0));
+  EXPECT_NO_THROW(PID pid(2.0, 2.0, 3.0, 1.0, 0.0, 0.0));
+
+  //  valid Ki
+  EXPECT_NO_THROW(PID pid(1.0, 0.2, 3.0, 1.0, 0.0, 0.0));
+  EXPECT_NO_THROW(PID pid(1.0, 2.0, 3.0, 1.0, 0.0, 0.0));
+
+  //  valid Kd
+  EXPECT_NO_THROW(PID pid(1.0, 2.0, 0.9, 1.0, 0.0, 0.0));
+  EXPECT_NO_THROW(PID pid(2.0, 2.0, 3.0, 1.0, 0.0, 0.0));
+}
+
 TEST(PIDTest, test_computeOutput) {
-  PID pid(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-  double output = pid.computeOutput(0, 1);
-  EXPECT_NEAR(4.0, output, 0.5);
+  PID pid(0.5, 0.4, 0.6, 1.0, -20.0, 20.0);
+
+  double output = pid.computeOutput(0, 5);
+  ASSERT_NEAR(7.5, output, 0.5);
+
+  output = pid.computeOutput(output, 5);
+  ASSERT_NEAR(-4.75, output, 0.5);
+
+  output = pid.computeOutput(output, 5);
+  ASSERT_NEAR(19.125, output, 0.5);
 }
 
-TEST(PIDTest, test_calculateErrorIntegral) {
-  PID pid(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-  double output = pid.calculateErrorIntegral(0, 1);
-  EXPECT_NEAR(0.0, output, 0.5);
+TEST(SaturationTest, test_computeOutput) {
+  PID pid1(10, 5, 45, 1.0, -20.0, 20.0);
+  PID pid2(10, 5, 45, 1.0, -20.0, 20.0);
+  ASSERT_LE(pid1.computeOutput(0, 5), 20);
+  ASSERT_GE(pid2.computeOutput(5, 0), -20);
 }
 
-TEST(PIDTest, test_calculateErrorDerivative) {
-  PID pid(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-  double output = pid.calculateErrorDerivative(0, 1);
-  EXPECT_NEAR(0.0, output, 0.5);
+TEST(ParamVerificationTest, verifyParameters) {
+  PID pid(10, 5, 45, 1.0, -20.0, 20.0);
+  ASSERT_EQ(pid.getKp(), 10);
+  ASSERT_EQ(pid.getKi(), 5);
+  ASSERT_EQ(pid.getKd(), 45);
 }
 
-// TEST(PIDTest, test_Kp) {
-//   PID pid(5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-// }
+TEST(IntegralTest, test_integral) {
+  PID pid(0.5, 0.4, 0.6, 1.0, -20.0, 20.0);
+  ASSERT_NEAR(pid.calculateErrorIntegral(5), 5, 0.5);
+}
+
+TEST(DerivativeTest, test_derivative) {
+  PID pid(0.5, 0.4, 0.6, 1.0, -20.0, 20.0);
+  ASSERT_NEAR(pid.calculateErrorDerivative(5), 5, 0.5);
+}
