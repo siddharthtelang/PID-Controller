f3e8cc9bd3074c474ae5d003bd350bffe1e5d59a
Siddharth Telang <stelang@umd.edu>
2021-10-01 04:48:34 -0400

    Add test module

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Siddharth Telang (stelang@umd.edu)
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief main file for test cases
+ * @version 0.1
+ * @date 2021-09-30
+ *
+ * Copyright (c) 2021 Pair A
+ *
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ test/test.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file test.cpp
+ * @author Siddharth Telang (stelang@umd.edu)
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief Test file for PID Controller
+ * @version 0.1
+ * @date 2021-09-30
+ *
+ * Copyright (c) 2021
+ *
+ */
+#include <gtest/gtest.h>
+#include <pid.hpp>
+
+TEST(PIDTest, test_computeOutput) {
+  PID pid;
+  double output = pid.computeOutput(0, 1);
+  EXPECT_NEAR(0.0, output, 0.5);
+}
+
+TEST(PIDTest, test_calculateErrorIntegral) {
+  PID pid;
+  double output = pid.calculateErrorIntegral(0, 1);
+  EXPECT_NEAR(1.0, output, 0.5);
+}
+
+TEST(PIDTest, test_calculateErrorDerivative) {
+  PID pid;
+  double output = pid.calculateErrorDerivative(0, 1);
+  EXPECT_NEAR(2.0, output, 0.5);
+}
