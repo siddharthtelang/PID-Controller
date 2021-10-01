db2c72af240c9ad951dff26e9519e6a03abf2f69
Siddharth Telang <stelang@umd.edu>
2021-10-01 10:16:37 -0400

    Modify tests

--- test/test.cpp
+++ test/test.cpp
@@ -13,19 +13,23 @@
 #include <pid.hpp>
 
 TEST(PIDTest, test_computeOutput) {
-  PID pid;
+  PID pid(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
   double output = pid.computeOutput(0, 1);
   EXPECT_NEAR(4.0, output, 0.5);
 }
 
 TEST(PIDTest, test_calculateErrorIntegral) {
-  PID pid;
+  PID pid(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
   double output = pid.calculateErrorIntegral(0, 1);
   EXPECT_NEAR(0.0, output, 0.5);
 }
 
 TEST(PIDTest, test_calculateErrorDerivative) {
-  PID pid;
+  PID pid(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
   double output = pid.calculateErrorDerivative(0, 1);
   EXPECT_NEAR(0.0, output, 0.5);
 }
+
+// TEST(PIDTest, test_Kp) {
+//   PID pid(5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+// }
